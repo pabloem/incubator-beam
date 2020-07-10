9c2d20d6295db6852d4381afb5b888d1f56f2064
Etta Rapp <ettarapp@google.com>
2020-07-10 02:44:51 +0000

    added getScheme and match methods to AzureBlobStoreFileSystem

--- sdks/java/io/azure/src/main/java/org/apache/beam/sdk/io/azure/blobstore/AzureBlobStoreFileSystem.java
+++ sdks/java/io/azure/src/main/java/org/apache/beam/sdk/io/azure/blobstore/AzureBlobStoreFileSystem.java
@@ -16,3 +16,75 @@
  * limitations under the License.
  */
 package org.apache.beam.sdk.io.azure.blobstore;
+
+import static org.apache.beam.vendor.guava.v26_0_jre.com.google.common.base.Preconditions.checkState;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import org.apache.beam.sdk.io.FileSystem;
+import org.apache.beam.sdk.io.fs.MatchResult;
+
+class AzureBlobStoreFileSystem extends FileSystem<azfsResourceId> {
+
+    @Override
+    protected String getScheme() {
+        return azfsResourceId.SCHEME;
+    }
+
+    @Override
+    protected List<MatchResult> match(List<String> specs) throws IOException {
+
+        List<azfsResourceId> paths = toAzfsPath(specs);
+
+        List<azfsResourceId> globs = new ArrayList<>();
+        List<azfsResourceId> nonGlobs = new ArrayList<>();
+        List<Boolean> isGlobBooleans = new ArrayList<>();
+
+        for (asfsResourceId path : paths) {
+            if (path.isWildcard()) {
+                globs.add(path);
+                isGlobBooleans.add(true);
+            } else {
+                nonGlobs.add(path);
+                isGlobBooleans.add(false);
+            }
+        }
+
+        Iterator<MatchResult> globMatches = matchGlobPaths(globs).iterator();
+        Iterator<MatchResult> nonGlobMatches = matchNonGlobPaths(nonGlobs).iterator();
+
+        ImmutableList.Builder<MatchResult> matchResults = ImmutableList.builder();
+        for (Boolean isGlob : isGlobBooleans) {
+            if (isGlob) {
+                checkState(globMatches.hasNext(), "Expect globMatches has next.");
+                matchResults.add(globMatches.next());
+            } else {
+                checkState(nonGlobMatches.hasNext(), "Expect nonGlobMatches has next.");
+                matchResults.add(nonGlobMatches.next());
+            }
+        }
+        checkState(!globMatches.hasNext(), "Expect no more elements in globMatches.");
+        checkState(!nonGlobMatches.hasNext(), "Expect no more elements in nonGlobMatches.");
+
+        return matchResults.build();
+    }
+
+    private List<azfsResourceId> toAzfsPaths(Collection<String> specs) {
+        // TODO
+        return None;
+    }
+
+    @VisibleForTesting
+    List<MatchResult> matchGlobPaths(Collection<azfsResourceId> globPaths) throws IOException {
+        // TODO
+        return None;
+    }
+
+    @VisibleForTesting
+    List<MatchResult> matchNonGlobPaths(Collection<azfsResourceId> globPaths) throws IOException {
+        // TODO
+        return None;
+    }
+}
